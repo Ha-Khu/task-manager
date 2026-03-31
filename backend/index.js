b751a94ed674eeb3aebf6a3ecb2b72f9f355a5b3
David <141168015+Ha-Khu@users.noreply.github.com>
2026-03-31 18:49:37 +0200

    feat: pridanie DELETE endpointu pre tasky

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const tasks = [
+let tasks = [
   {id: 1, text: "Nakúpiť potraviny", done: false},
   {id: 2, text: "Spraviť domácu úlohu", done: false}
 ]
@@ -16,6 +16,12 @@ app.use(express.json())
 app.post('/tasks', (req, res) => {
   const data = req.body
   tasks.push(data)
+  res.json(tasks)
+})
+
+app.delete('/tasks/:id', (req, res) =>{
+  const deleteingTask = Number(req.params.id) 
+  tasks = tasks.filter((task) => task.id !== deleteingTask)
   res.json(tasks)
 })
 
