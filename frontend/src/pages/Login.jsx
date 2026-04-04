874473708653cb571e0e84a6166ffab32992fedf
David <141168015+Ha-Khu@users.noreply.github.com>
2026-04-04 19:03:14 +0200

    feat: pridanie Login formulára s email a password inputmi

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,22 @@
-
+import {useState} from 'react'
 function Login(){
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
   return (
     <div>
       <h1>Login</h1>
+
+      <input 
+      type="email"
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+      placeholder="email" />
+      <input 
+      type="password"
+      value={password}
+      onChange={(e) => setPassword(e.target.value)} 
+      placeholder="password" />
+      <button>Poslať</button>
     </div>
   )
 }
