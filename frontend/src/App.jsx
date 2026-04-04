1ce40255518f319f0e4c02e7d256a7208af79094
David <141168015+Ha-Khu@users.noreply.github.com>
2026-04-04 18:21:27 +0200

    refactor: presunutie taskov do pages/Tasks.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,75 +1,8 @@
-import {useState, useEffect} from 'react'
-import axios from 'axios'
-import TaskItem from './TaskItem'
 
-
-function App() {
-  const [tasks, setTasks] = useState([])
-  const [inputValue, setInputValue] = useState("")
-
-  function addTask() {
-    if(inputValue === "") return
-    axios.post("http://localhost:3000/tasks", {text: inputValue, done: false})
-    .then(function(response){
-      axios.get("http://localhost:3000/tasks").then(function(response){
-        setTasks(response.data)
-      })
-      setInputValue("")
-    })
-  }
-
-  function deleteTask(id){
-    axios.delete(`http://localhost:3000/tasks/${id}`).
-    then(function(response){
-      axios.get("http://localhost:3000/tasks").then(function(response){
-        setTasks(response.data)
-      })
-    })
-  }
-
-  function toggleTask(id, done){
-    axios.put(`http://localhost:3000/tasks/${id}`, {done: !done}).
-    then(function(response) {
-      axios.get("http://localhost:3000/tasks").then(function(response) {
-        setTasks(response.data)
-      })
-    })
-  }
-
-  const [isLoaded, setIsLoaded] = useState(false)
-
-  useEffect(() =>{
-    axios.get("http://localhost:3000/tasks").then(function(response){
-      setTasks(response.data)
-      setIsLoaded(true)
-    })
-  }, [])
-
-  useEffect(() => {
-    if (!isLoaded) return
-    console.log("ukladám tasky: ", tasks)
-    localStorage.setItem("tasks", JSON.stringify(tasks))
-  }, [tasks, isLoaded])
-
-  return(
+function App(){
+  return (
     <div>
-      <h1>Task Manager</h1>
-      <input 
-      type="text" 
-      value={inputValue}
-      onChange={(e) => setInputValue(e.target.value)}
-      placeholder='Nový task...'
-      />
-      <button onClick={addTask}>Pridať</button>
-      {tasks.map((task, index) =>(
-        <TaskItem 
-        key={index} 
-        text={task.text}
-        done={task.done} 
-        onDelete={() => deleteTask(task.id)} 
-        onToggle={() => toggleTask(task.id, task.done)}
-        />
-      ))}
+      <h1>App</h1>
     </div>
   )
 }
